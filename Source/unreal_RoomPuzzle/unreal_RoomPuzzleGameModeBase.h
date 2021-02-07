d78ecafd1997eb75768e81c1938ca402c1e63a67
areizas <areizas96@gmail.com>
2021-02-07 00:21:25 -0500

    First commit

---
+++ Source/unreal_RoomPuzzle/unreal_RoomPuzzleGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "unreal_RoomPuzzleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_ROOMPUZZLE_API Aunreal_RoomPuzzleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
